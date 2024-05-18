3ce9f21a0fe8348d1ce056d6dc29991ac09980e1
santhosh.chintala <santhoshsai80@gmail.com>
2024-05-18 12:56:13 +0530

    update

--- modules/provider.tf
+++ modules/provider.tf
@@ -10,6 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA2UC27QQPJWN5ZX7V"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
